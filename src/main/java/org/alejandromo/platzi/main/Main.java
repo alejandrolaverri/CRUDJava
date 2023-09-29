e558b4866a6605a8195f98292e68b1b9d921dff1
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-09-29 19:04:54 +0200

    Optimización de recursos

--- src/main/java/org/alejandromo/platzi/main/Main.java
+++ src/main/java/org/alejandromo/platzi/main/Main.java
@@ -5,28 +5,18 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection myConn = null; // Nuestro objeto de conexion
-        Statement myStamt = null; // Objeto Statement
-        ResultSet myRes = null; // Para procesar nuestros resultados
-
         String url = "jdbc:mysql://localhost:3306/project";
         String user = "root";
         String pass = "1234";
 
-        try {
-            // 1. Conexion a nuesta base de datos, pasando 3 argumentos
-            myConn = DriverManager.getConnection(url, user, pass);
+        try (Connection myConn = DriverManager.getConnection(url, user, pass);
+             Statement myStamt = myConn.createStatement();
+             ResultSet myRes = myStamt.executeQuery("select * from employees")) {
 
-            // 2 . crear una declaracion obejto
-            myStamt = myConn.createStatement();
-
-            //3. Ejecutar la consulta Sql
-            myRes = myStamt.executeQuery("select * from employees");
-
-            while (myRes.next()){
+            while (myRes.next()) {
                 System.out.println(myRes.getString("first_name"));
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Algo salio mal ");
         }
