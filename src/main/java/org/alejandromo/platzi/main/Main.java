3c02edc81bd41991be5068ff8d6fbf39afe8dc57
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-10-12 12:20:45 +0200

    Visualizando bases de datos con Java Swing

--- src/main/java/org/alejandromo/platzi/main/Main.java
+++ src/main/java/org/alejandromo/platzi/main/Main.java
@@ -4,6 +4,7 @@ import org.alejandromo.platzi.model.Employee;
 import org.alejandromo.platzi.repository.EmployeeRepository;
 import org.alejandromo.platzi.repository.Repository;
 import org.alejandromo.platzi.util.DatabaseConnection;
+import org.alejandromo.platzi.view.SwingApp;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,40 +12,8 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
-        try (Connection myConn = DatabaseConnection.getInstance()) {
-             Repository<Employee> repository = new EmployeeRepository();
-
-            System.out.println("---Listando---");
-            repository.findAll().forEach(System.out::println);
-
-            System.out.println("---Insertamos un empleado---");
-            Employee employee = new Employee();
-            employee.setFirst_name("Diego");
-            employee.setPa_surname("Pimentel");
-            employee.setMa_surname("Gutierrez");
-            employee.setEmail("diego@example.com");
-            employee.setSalary((float)19000);
-            repository.save(employee);
-            System.out.println(employee);
-
-            System.out.println("---Actualizando un empleado---");
-            Employee employee2 = new Employee();
-            employee2.setId(3);
-            employee2.setFirst_name("Michael");
-            employee2.setPa_surname("Johnson");
-            employee2.setMa_surname("Browny");
-            employee2.setEmail("emily.browny@example.com");
-            employee2.setSalary((float)55000);
-            repository.save(employee2);
-            System.out.println(employee2);
-
-            System.out.println("---Empleado elminado---");
-            repository.delete(18);
-
-            System.out.println("---Listando---");
-            repository.findAll().forEach(System.out::println);
-        }
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
     }
 
 }
